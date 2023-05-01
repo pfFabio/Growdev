7130807ce9cd031045d821abc1b91e8a4d7dac28
fabio pinto figueiredo <pf95fabio@gmail.com>
2023-05-01 01:25:35 -0300

    correções

--- semana 2-2/ex_16.py
+++
@@ -1,7 +0,0 @@
-"""
-Escreva um programa que conte em ordem reversa, de 25 a zero.
-"""
-
-
-for i in range(25,0, -1):
-    print(i)
--- semana 2/ex_11.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-while True:
-    ano = int(input("digite o ano com 4 digitos"))
-    mes = int(input("digite o mês com 2 digitos"))
-    dia = int(input("digite os dias com 2 digitos"))
-    if datetime.datetime(year=ano, month=mes, day=dia):
-        print("ano válido")
