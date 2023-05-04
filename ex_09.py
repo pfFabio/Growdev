a4ba0b6a594a56c8c849966505c5f46263cfd6eb
fabio pinto figueiredo <pf95fabio@gmail.com>
2023-05-04 10:58:46 -0300

    exercicio 9, forca

--- ex_09.py
+++ ex_09.py
@@ -10,7 +10,11 @@ Palpite: d
 Saída: d _ d _ _
 Se completar a frase o usuário ganha o jogo, se sua p
 """
+import random
 
+contador, pontos= 0, 0
+palavra = []
+tela = ['_', '_', '_', '_', '_']
 
 
 
@@ -24,4 +28,33 @@ for i in range(len(conteudo)):
     conteudo[i] = conteudo[i].replace('\n', '')
 
 print(conteudo)
+termo = conteudo[random.randint(1, len(conteudo))]
 
+for i in termo:
+    palavra += [i]
+
+print(termo)
+
+print('Bem vindo ao jogo da forca:')
+while contador < 5:
+    print(f'vc tem {pontos} pontos')
+    acerto = 0
+    printa = ''
+    print('\na palavra é: ' + termo)
+    tentativa = input('\ndigite uma letra: ')
+
+    for i in range(len(palavra)):
+        if tentativa == palavra[i]:
+            tela[i] = tentativa
+            contador += 1
+            acerto = 1
+    if acerto == 1:
+        print("você acertou e ganhou 1 ponto")
+        for i in tela:
+            printa += i
+        print(printa)
+        pontos += 1
+    else:
+        print('você errou e perdeu 1 ponto')
+        pontos -= 1
+print('Parabéns você ganhou!')
